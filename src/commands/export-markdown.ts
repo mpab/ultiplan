2330cf9246c7b23e6c0ef589cd1f34661b8837c9
mpab <mick_bythell@hotmail.com>
2022-05-22 20:09:31 +0200

    reporting: create parameterized summary reports per project (output to markdown)

--- src/commands/export-markdown.ts
+++ src/commands/export-markdown.ts
@@ -4,6 +4,9 @@ import { DbRecord } from "../db/db-record";
 import { dbHandle } from "../db/db-util";
 
 module.exports = () => {
+  const minimist = require(`minimist`);
+  const args = minimist(process.argv.slice(2));
+
   const fs = require("fs");
   fs.readFile(dbHandle, function (err: any, data: string) {
     if (err) {
@@ -13,17 +16,38 @@ module.exports = () => {
     console.log("# PROJECTS");
     console.log("");
 
-    let project = "ultiplan";
-
     const unfilteredRecords: DbRecord[] = JSON.parse(data);
+    let project_name = args.project || args.p || undefined;
 
+    if (project_name) {
+      const records = unfilteredRecords.filter(record => record.project === project_name);
+      report(records, project_name);
+      console.log("");
+      console.log("---");
+      console.log("");
+      return;
+    }
+  
+    // extract all projects & iterate
+    const projects = [...new Set(unfilteredRecords.map(record => record.project))];
+
+    for (project_name of projects) {
+      const records = unfilteredRecords.filter(record => record.project === project_name);
+      report(records, project_name);
+      console.log("");
+      console.log("---");
+      console.log("");
+    }    
+  });
+
+  const report = (project_records: DbRecord[], project: string) => {
     console.log("## project: " + project);
     console.log("");
 
-    const todo: DbRecord[] = unfilteredRecords
+    const todo: DbRecord[] = project_records
       .filter(record => record.project === project && !record.completed_on);
     
-    const done: DbRecord[] = unfilteredRecords
+    const done: DbRecord[] = project_records
       .filter(record => record.project === project && record.completed_on);
 
     console.log("### todo: " + project);
@@ -33,7 +57,8 @@ module.exports = () => {
     console.log("### done: " + project);
     console.log("");
     for (let record of done) console.log(format(record));
-  });
+
+  }
 
   const format = (record: DbRecord) => {
     return "- " + (record.completed_on ? record.completed_on + ": " : "") + record.description;
