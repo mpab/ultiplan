17469606160a4dac69d9bdd50926f124de413524
mpab <mick_bythell@hotmail.com>
2022-07-03 15:47:59 +0200

    wip: add task dialog

--- ui/src/components/Table.jsx
+++ ui/src/components/Table.jsx
@@ -2,6 +2,15 @@ import React, { useState, useEffect } from "react";
 import MaterialTable, { Column } from "@material-table/core";
 import tableIcons from "./TableIcons";
 import { Container } from "@material-ui/core";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 import { TaskRecord } from "../types";
 
 type Props = {
@@ -25,14 +34,23 @@ const options = {
   pageSizeOptions: [10, 20, 50],
 };
 
+
 export const Table = ({ data }) => {
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isDialogOpen, setIsDialogOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsDialogOpen(false);
+  };
 
   useEffect(() => {
     // Closes dialog after saving
-    if (isDialogOpen) {
-      setIsDialogOpen(false);
-    }
+    // if (isDialogOpen) {
+    //   setIsDialogOpen(false);
+    // }
   }, [data, isDialogOpen]);
 
   return (
@@ -53,10 +71,31 @@ export const Table = ({ data }) => {
             icon: tableIcons.Add,
             tooltip: "Add Task",
             isFreeAction: true,
-            onClick: (event) => setIsDialogOpen(true),
+            onClick: (event) => handleClickOpen(),
           },
         ]}
       />
+      <Dialog open={isDialogOpen} onClose={handleClose}>
+        <DialogTitle>Add a new Task</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Enter a description
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="description"
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Add</Button>
+        </DialogActions>
+      </Dialog>
     </Container>
   );
 };
