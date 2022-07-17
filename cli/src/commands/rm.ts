056a2f78ac9ee1e1e375ee52e40d931769808930
mpab <mick_bythell@hotmail.com>
2022-07-17 23:54:30 +0200

    improved logging

--- cli/src/commands/rm.ts
+++ cli/src/commands/rm.ts
@@ -5,5 +5,5 @@ import dbDelete from 'ultiplan-api/src/libs/db/db-delete-record';
 module.exports = async (handle: string) => {
   let id: string = process.argv.slice(3).join(` `);
   if (!id.length) return;
-  console.log(dbDelete(id, handle)? 'deleted' : 'not found');
+  console.log(dbDelete(id, handle)? `deleted ${id}` : `${id} not found`);
 };
